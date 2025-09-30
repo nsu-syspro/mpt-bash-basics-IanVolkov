46f36da1a741925c31a936a029ff9ce75e72f899
Ian Volkov <y.volkov@g.nsu.ru>
2025-09-30 22:53:21 +0700

    fixed shellcheck

--- solution/fizzbuzz.sh
+++ solution/fizzbuzz.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 for ((i = 1 ; i <= "$1" ; i++)); do
-  x=$(($i % 3))
-  y=$(($i % 5))
+  x=$((i % 3))
+  y=$((i % 5))
   if [[ x -eq 0 && y -eq 0 ]]; then
     echo "Fizz Buzz"
   elif [[ x -eq 0 ]]; then
